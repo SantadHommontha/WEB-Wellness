e624589fc2eb06ca82fd018dddfac04cc4e3f9a8
MineMineMine221 <114082225+MineMineMine221@users.noreply.github.com>
2025-04-11 17:55:51 +0700

    Test Javascript

---
+++ Script/sideBar.js
@@ -0,0 +1,14 @@
+
+
+const toggleButton = document.getElementById('toggle_btn');
+const sidebar = document.getElementById('Sidebar');
+
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+    
+});
+
+
+
+
+
