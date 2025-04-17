719ff3bca1942e565552ddc76e97a77c47e0f9a8
SantadHommontha <santadhommontha@gmail.com>
2025-04-17 15:28:14 +0700

    side bar

--- Script/side_Bar.js
+++ Script/side_Bar.js
@@ -1,12 +1,33 @@
 
 
-// const toggleButton = document.getElementById('toggle_btn');
-// const sidebar = document.getElementById('Sidebar');
+function SideBarToggle() {
 
-// toggleButton.addEventListener('click', () => {
-//     sidebar.classList.toggle('open');
-    
-// });
+    console.log("Side Bar Toggle");
+    const sidebar = document.getElementById('Sidebar');
+
+    setTimeout(() => {
+        sidebar.classList.toggle('open');
+    }, 0.1);
+
+
+}
+
+
+
+
+
+document.addEventListener('click', function (event) {
+
+    const sidebar = document.getElementById('Sidebar');
+    if (!sidebar.contains(event.target)) {
+        if (sidebar.classList.contains('open')) {
+            sidebar.classList.remove('open');
+            console.log('close side bar');
+        }
+    }
+});
+
+
 
 
 
