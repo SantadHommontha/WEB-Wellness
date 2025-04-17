4b9c0f843207b36ad6402fd0566ac2ac2c2fbb82
SantadHommontha <santadhommontha@gmail.com>
2025-04-17 18:16:16 +0700

    up

---
+++ Script/Choose_Product.js
@@ -0,0 +1,25 @@
+
+
+
+function SuperFood_Select() {
+    let p1 = document.getElementById('WELLNESSX');
+    let p2 = document.getElementById('NIMNIM');
+    let p3 = document.getElementById('KHAOPLAWAAN');
+
+    let p1_child = p1.children;
+    let p2_child = p2.children;
+    let p3_child = p3.children;
+
+    p1_child[0].style.opacity = '0';
+    p1_child[1].style.opacity = '1';
+
+}
+
+
+function Supplementary_Select() {
+
+}
+
+function Services_Select() {
+
+}
