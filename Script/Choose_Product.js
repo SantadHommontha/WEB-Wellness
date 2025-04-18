d8420b6ccbce65d500d2c624ee9412878e6f2205
MineMineMine221 <114082225+MineMineMine221@users.noreply.github.com>
2025-04-18 13:31:38 +0700

    Product Select

--- Script/Choose_Product.js
+++ Script/Choose_Product.js
@@ -1,5 +1,11 @@
 
 
+function NewSrc(currentSrc) {
+    let scc = currentSrc.split('?')[0];
+    let newSrc = `${scc}?reload=${new Date().getTime()}`;
+    return newSrc;
+}
+
 
 function SuperFood_Select() {
     let p1 = document.getElementById('WELLNESSX');
@@ -12,14 +18,111 @@ function SuperFood_Select() {
 
     p1_child[0].style.opacity = '0';
     p1_child[1].style.opacity = '1';
+    p1_child[1].src = NewSrc(p1_child[1].src);
+    p1_child[2].style.opacity = '1';
+
+    p2_child[0].style.opacity = '0';
+    p2_child[1].style.opacity = '1';
+    p2_child[1].src = NewSrc(p2_child[1].src);
+    p2_child[2].style.opacity = '1';
+
+    p3_child[0].style.opacity = '0';
+    p3_child[1].style.opacity = '1';
+    p3_child[1].src = NewSrc(p3_child[1].src);
+    p3_child[2].style.opacity = '1';
+
+}
+
+function SuperFood_UnSelect() {
+
+    let p1 = document.getElementById('WELLNESSX');
+    let p2 = document.getElementById('NIMNIM');
+    let p3 = document.getElementById('KHAOPLAWAAN');
+    console.log("FFFFFFFFFFFFFFF");
+    let p1_child = p1.children;
+    let p2_child = p2.children;
+    let p3_child = p3.children;
+
+    p1_child[0].style.opacity = '1';
+    p1_child[1].style.opacity = '0';
+    p1_child[2].style.opacity = '0';
+
+    p2_child[0].style.opacity = '1';
+    p2_child[1].style.opacity = '0';
+    p2_child[2].style.opacity = '0';
+
+    p3_child[0].style.opacity = '1';
+    p3_child[1].style.opacity = '0';
+    p3_child[2].style.opacity = '0';
 
 }
 
 
 function Supplementary_Select() {
+    let p1 = document.getElementById('WAFFENHUND');
+    let p2 = document.getElementById('PIMOL');
+
+    let p1_child = p1.children;
+    let p2_child = p2.children;
+
+    p1_child[0].style.opacity = '0';
+    p1_child[1].style.opacity = '1';
+    p1_child[1].src = NewSrc(p1_child[1].src);
+    p1_child[2].style.opacity = '1';
+
+    p2_child[0].style.opacity = '0';
+    p2_child[1].style.opacity = '1';
+    p2_child[1].src = NewSrc(p2_child[1].src);
+    p2_child[2].style.opacity = '1';
+
 
 }
 
-function Services_Select() {
 
+function Supplementary_UnSelect() {
+
+    let p1 = document.getElementById('WAFFENHUND');
+    let p2 = document.getElementById('PIMOL');
+
+    let p1_child = p1.children;
+    let p2_child = p2.children;
+
+
+    p1_child[0].style.opacity = '1';
+    p1_child[1].style.opacity = '0';
+    p1_child[2].style.opacity = '0';
+
+    p2_child[0].style.opacity = '1';
+    p2_child[1].style.opacity = '0';
+    p2_child[2].style.opacity = '0';
+
+
+
+}
+
+
+
+function Services_Select() {
+    let p1 = document.getElementById('House_Button');
+   
+    let p1_child = p1.children;
+
+    p1_child[0].style.opacity = '0';
+    p1_child[1].style.opacity = '1';
+    p1_child[2].style.opacity = '0';
+
+  
+}
+
+function Services_UnSelect() {
+    let p1 = document.getElementById('House_Button');
+   
+    let p1_child = p1.children;
+   
+
+    p1_child[0].style.opacity = '1';
+    p1_child[1].style.opacity = '0';
+    p1_child[2].style.opacity = '0';
+
+ 
 }
--- Script/side_Bar.js
+++ Script/side_Bar.js
@@ -1,5 +1,10 @@
 
 
+
+
+
+
+
 function SideBarToggle() {
 
     console.log("Side Bar Toggle");
@@ -7,6 +12,14 @@ function SideBarToggle() {
 
     setTimeout(() => {
         sidebar.classList.toggle('open');
+
+        let houseBtn = document.getElementById('House_Button')
+        let allChild = houseBtn.children;
+
+        allChild[0].style.opacity = '0';
+        allChild[1].style.opacity = '0';
+        allChild[2].style.opacity = '1';
+
     }, 0.1);
 
 
@@ -23,6 +36,15 @@ document.addEventListener('click', function (event) {
         if (sidebar.classList.contains('open')) {
             sidebar.classList.remove('open');
             console.log('close side bar');
+
+            let houseBtn = document.getElementById('House_Button')
+            let allChild = houseBtn.children;
+
+
+            allChild[0].style.opacity = '1';
+            allChild[1].style.opacity = '0';
+            allChild[2].style.opacity = '0';
+
         }
     }
 });
